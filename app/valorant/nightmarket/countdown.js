cacb7bfd341a1c1cfd28c2c34a3332000ae0053d
Katsuyu <antoniusdarren07@gmail.com>
2022-04-06 08:43:17 +0700

    Fix: Dont display if value 0

--- app/valorant/nightmarket/countdown.js
+++ app/valorant/nightmarket/countdown.js
@@ -16,8 +16,18 @@ var x = setInterval(function () {
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
   // Display the result in the element with id="demo"
-  document.getElementById("countdown").innerHTML =
-    days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+  if (days > 0) {
+    document.getElementById("countdown").innerHTML =
+      days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+  } else if (hours > 0) {
+    document.getElementById("countdown").innerHTML =
+      hours + "h " + minutes + "m " + seconds + "s ";
+  } else if (minutes > 0) {
+    document.getElementById("countdown").innerHTML =
+      minutes + "m " + seconds + "s ";
+  } else {
+    document.getElementById("countdown").innerHTML = seconds + "s ";
+  }
 
   // If the count down is finished, write some text
   if (distance < 0) {
